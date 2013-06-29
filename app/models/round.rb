5adcbdcc3d59c532aa8a2f107e340c8a5e963dfc
Apprentice <apprentice@devbootcamp.com>
2013-06-28 18:24:07 -0700

    finished game

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,14 @@
 class Round < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
-  has_many :guesses, dependent: :destroy
+  belongs_to :deck
+  has_many :guesses, :dependent => :destroy
+
+  def number_correct
+    self.guesses.where(is_correct: true).count 
+  end
+
+  def number_incorrect
+    self.guesses.where(is_correct: false).count 
+  end
 end
