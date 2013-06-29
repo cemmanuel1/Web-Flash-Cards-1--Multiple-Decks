91a0a938868fdf3db8a3fbfcfc7da43c6b89f667
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-06-29 15:17:53 -0700

    css'd dashboard

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -6,6 +6,8 @@ end
 get '/round/:round_id/flipcard' do
   @round = Round.find(params[:round_id])
   @card = get_unseen_card(@round)
+  p "CARD BELOW"
+  p @card
   redirect "/round/#{@round.id}/results" unless @card
   @guess = Guess.new
   erb :show_card
@@ -20,7 +22,7 @@ post '/round/:round_id/card/:card_id/submit' do
 end
 
 get '/round/:round_id/results' do
-  @round = Round.find(params[:round_id])
+@round = Round.find(params[:round_id])
   erb :round_results
 end
 
